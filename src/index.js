2bab9a358ea3c60ab608784fd3355659eb4ed0cf
hariharan <hariharan.1985.s@gmail.com>
2022-08-23 22:19:04 +0530

    modified port

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ app.post('/submit', function (req, res) {
       res.send({response:'Your Order Submitted Successfully'});   
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
